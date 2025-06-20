85a44606c8097ffc7813b3eb58568930e21aa865
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 06:07:02 +0000

    [automatic commit] 2025-06-20 06:07:02 UTC 203.220.82.245

--- part1/app.js
+++ part1/app.js
@@ -49,7 +49,7 @@ let db;
 
         // creating tables
 
-        await db.query(`
+        await db.execute(`
             CREATE TABLE Users (
                 user_id INT AUTO_INCREMENT PRIMARY KEY,
                 username VARCHAR(50) UNIQUE NOT NULL,
@@ -60,7 +60,7 @@ let db;
             )
         `);
 
-        await db.query(`
+        await db.execute(`
             CREATE TABLE Dogs (
                 dog_id INT AUTO_INCREMENT PRIMARY KEY,
                 owner_id INT NOT NULL,
