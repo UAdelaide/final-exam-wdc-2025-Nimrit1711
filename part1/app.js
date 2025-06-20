7eabc33bf0fbc2ee82c8af5be965be5e8f91abde
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 06:18:46 +0000

    [automatic commit] 2025-06-20 06:18:46 UTC 203.220.82.245

--- part1/app.js
+++ part1/app.js
@@ -165,6 +165,8 @@ let db;
                     d.name AS dog_name,
                     d.size,
                     u.username AS owner_username
+                FROM Dogs d
+                JOIN Users u ON d.owner_id
                 ');
             res.json(dogs);
         } catch (err) {
