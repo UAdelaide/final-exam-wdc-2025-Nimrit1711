05c690e802c72039afdf8b11ba95e13e753e21be
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 10:24:48 +0000

    [automatic commit] 2025-06-20 10:24:48 UTC 203.220.82.245

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -58,7 +58,7 @@ router.post('/login', async (req, res) => {
       }
     } catch (error) {
       console.error(error);
-      res.status(500).send('Server  error');
+      res.status(500).send('Login Server error');
     }
 });
 
