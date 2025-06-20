10d489190a01972dacf452664033050ba8d8f61f
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:42:50 +0000

    [automatic commit] 2025-06-20 05:42:50 UTC 203.220.82.245

--- part1/app.js
+++ part1/app.js
@@ -52,6 +52,20 @@ app.use('/users', usersRouter);
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
         `);
-})
+
+        await db.query(`
+            CREATE TABLE Users (
+                user_id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                password_hash VARCHAR(255) NOT NULL,
+                role ENUM('owner', 'walker') NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `);
+
+
+
+});
 
 module.exports = app;
