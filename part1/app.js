b55cc37bc670ab14052ad5b79a0760e54761a826
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:41:48 +0000

    [automatic commit] 2025-06-20 05:41:48 UTC 203.220.82.245

--- part1/app.js
+++ part1/app.js
@@ -43,14 +43,7 @@ app.use('/users', usersRouter);
         await db.query(`DROP TABLE IF EXISTS Users`);
 
         await db.query(`
-      CREATE TABLE Users (
-        user_id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL,
-        password_hash VARCHAR(255) NOT NULL,
-        role ENUM('owner', 'walker') NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      )
+     
     `);
 })
 
