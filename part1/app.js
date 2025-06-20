56770ca1b1fa1a8697836580060a57ee32c14163
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:50:29 +0000

    [automatic commit] 2025-06-20 05:50:29 UTC 203.220.82.245

--- part1/app.js
+++ part1/app.js
@@ -122,11 +122,13 @@ app.use('/users', usersRouter);
             INSERT INTO Dogs (owner_id, name, size)
                 VALUES
                 ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
-    -> ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
-    -> ((SELECT user_id FROM Users WHERE username='nimx2'), 'Oreo', 'small'),
-    -> ((SELECT user_id FROM Users WHERE username='nimx2'), 'Princess', 'large'),
-    -> ((SELECT user_id FROM Users WHERE username='nimx2'), 'Cupcake', 'large');
+                ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
+                ((SELECT user_id FROM Users WHERE username='nimx2'), 'Oreo', 'small'),
+                ((SELECT user_id FROM Users WHERE username='nimx2'), 'Princess', 'large'),
+                ((SELECT user_id FROM Users WHERE username='nimx2'), 'Cupcake', 'large');
         `);
+
+        
 
 
     });
