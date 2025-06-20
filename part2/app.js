b16aa99ed564b9463f8c36bcad86f10a11a702f2
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 11:48:35 +0000

    [automatic commit] 2025-06-20 11:48:35 UTC 203.220.82.245

--- part2/app.js
+++ part2/app.js
@@ -40,7 +40,7 @@ function requireRole(role) {
 }
 
 // Now user needs to login in order to access these pages
-app.get('/owner-dashboard.html', requireLogin, requireRole('owner'), (req, res) => {
+app.get('/owner', requireLogin, requireRole('owner'), (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'owner-dashboard.html'));
 });
 
