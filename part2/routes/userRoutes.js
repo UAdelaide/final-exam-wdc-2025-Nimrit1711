d98de9bb579d8187e84e9be591fa3549f85397ff
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 10:19:06 +0000

    [automatic commit] 2025-06-20 10:19:06 UTC 203.220.82.245

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -40,7 +40,7 @@ router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const [rows] = await db.execute(`SELECT * FROM Users WHERE username = ?
+    const [rows] = await db.execute(`SELECT * FROM Users WHERE username = ? AND password_hash = ?`, 
       SELECT user_id, username, role FROM Users
       WHERE email = ? AND password_hash = ?
     `, [email, password]);
