7441643595ba78070eae4e4c0e4891c65b3eab63
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 06:28:03 +0000

    [automatic commit] 2025-06-20 06:28:03 UTC 203.220.82.245

--- part1/app.js
+++ part1/app.js
@@ -196,4 +196,27 @@ let db;
             }
         });
 
+
+        // Route to /api/walkers/summary
+        app.get('/api/walkers/summary', async (req, res) => {
+            try {
+                const [opens] = await db.execute(`
+                    SELECT
+                        wr.request_id,
+                        d.name AS dog_name,
+                        wr.requested_time,
+                        wr.duration_minutes,
+                        wr.location,
+                        u.username AS owner_username
+                    FROM WalkRequests wr
+                    JOIN Dogs d ON wr.dog_id = d.dog_id
+                    JOIN Users u ON d.owner_id = u.user_id
+                    WHERE wr.status = 'open'
+                    `);
+                res.json(opens);
+            } catch (err) {
+                res.status(500).json({ error: 'Failed to fetch walk requests for status = open' });
+            }
+        });
+
 module.exports = app;
