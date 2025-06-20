fa1312130b093f2924aa1b29f8e16b8e22d53b75
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 11:36:30 +0000

    [automatic commit] 2025-06-20 11:36:30 UTC 203.220.82.245

--- part2/app.js
+++ part2/app.js
@@ -20,7 +20,11 @@ app.use(session({
 }));
 
 //middle ware for user authentication
-function requireLogin(req, res, next)
+function requireLogin(req, res, next) {
+    if (req.session && req.session.user) {
+        
+    }
+}
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
